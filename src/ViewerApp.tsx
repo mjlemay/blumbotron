8c6571760b29235dc1f315c046789a5e484abbf2
Michael LeMay <mjlemay@gmail.com>
2025-05-28 10:00:21 -0700

    adds linting rules that are streaming friendly

--- src/ViewerApp.tsx
+++ src/ViewerApp.tsx
@@ -2,16 +2,15 @@ import "./App.css";
 import FullScreenViewer from "./components/fullScreenViewer.tsx";
 
 function ViewerApp() {
-
   return (
     <div
-    className={`flex w-screen bg-neutral-900 text-white select-none`}
-    data-theme={"darkTheme"}
-  >
-    <div className={`flex flex-1 items-center justify-center`}>
+      className={`flex w-screen bg-neutral-900 text-white select-none`}
+      data-theme={"darkTheme"}
+    >
+      <div className={`flex flex-1 items-center justify-center`}>
         <FullScreenViewer isFullScreen />
+      </div>
     </div>
-  </div>
   );
 }
 
